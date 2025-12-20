9729806f87e322c09f699ba8bca4e7b339a4cd88
DARKPINGGD <omerasarfino@gmail.com>
2025-12-20 19:46:54 +0300

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Pointercrate Clone",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="tr">
+      <body>{children}</body>
+    </html>
+  );
+}
